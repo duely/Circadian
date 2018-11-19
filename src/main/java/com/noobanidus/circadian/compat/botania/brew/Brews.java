2fd9298b70828f3929099e10d4b2ab12304a4bbe
Jon McManus <due@wxwhatever.com>
2018-11-20 09:30:05 +1000

    Properly add recipe for Wings brew.

--- src/main/java/com/noobanidus/circadian/compat/botania/brew/Brews.java
+++ src/main/java/com/noobanidus/circadian/compat/botania/brew/Brews.java
@@ -1,14 +1,25 @@
 package com.noobanidus.circadian.compat.botania.brew;
 
 import WayofTime.bloodmagic.core.RegistrarBloodMagic;
+import WayofTime.bloodmagic.item.types.ComponentTypes;
+import com.noobanidus.circadian.Circadian;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.brew.Brew;
+import vazkii.botania.common.item.ModItems;
 
 public class Brews {
     public static void registerBrews () {
-        Brew flight_brew = new Brew("flight", "Wings", 0x99ffff, 150000, new PotionEffect(RegistrarBloodMagic.FLIGHT, 6000));
+        Brew flight_brew = new Brew("flight", "circadian.brew.Wings", 0x99ffff, Circadian.CONFIG.get("Items.FlightBrew", "Cost", 50000, "Mana cost of the Wings flight brew."), new PotionEffect(RegistrarBloodMagic.FLIGHT, 6000));
 
         BotaniaAPI.registerBrew(flight_brew);
+
+        Item angelicFeather = Item.REGISTRY.getObject(new ResourceLocation("xreliquary", "angelic_feather"));
+
+        BotaniaAPI.registerBrewRecipe(flight_brew, new ItemStack(ModItems.manaResource, 1, 18), ComponentTypes.REAGENT_AIR.getStack(), new ItemStack(Items.GHAST_TEAR, 1, 0), new ItemStack(angelicFeather, 1, 0));
     }
 }
