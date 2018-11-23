8c90735cbc9d8e884e27881be67a5983347cf01b
Jon McManus <due@wxwhatever.com>
2018-11-23 14:43:31 +1000

    Tweak dependencies.

--- src/main/java/com/noobanidus/circadian/Circadian.java
+++ src/main/java/com/noobanidus/circadian/Circadian.java
@@ -36,7 +36,7 @@ public class Circadian {
     public static final String MODID = "circadian";
     public static final String MODNAME = "Circadian";
     public static final String VERSION = "1.0.0";
-    public static final String DEPENDS = "required-after:twilightforest;required-after:thaumcraft;required-after:thermalfoundation;required-after:theoneprobe;required-after:astralsorcery;required-after:agricraft;required-after:oreberries;required-after:bloodmagic;before:jei";
+    public static final String DEPENDS = "required-after:twilightforest;required-after:thaumcraft;required-after:thermalfoundation;required-after:theoneprobe;required-after:astralsorcery;required-after:agricraft;required-after:oreberries;required-after:bloodmagic;required-after:thermalexpansion;before:jei;";
 
     public final static Logger LOG = LogManager.getLogger(MODID);
     public final static ConfigHandler CONFIG = new ConfigHandler(VERSION);
