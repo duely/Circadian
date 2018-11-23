8698a0dc8f919ed662f512d358015e6215afe2a1
Jon McManus <due@wxwhatever.com>
2018-11-23 14:43:48 +1000

    Add some OreDictionary entries for use in Blood Magic meteors.

--- src/main/java/com/noobanidus/circadian/compat/OreDictionaryEntries.java
+++ src/main/java/com/noobanidus/circadian/compat/OreDictionaryEntries.java
@@ -1,5 +1,6 @@
 package com.noobanidus.circadian.compat;
 
+import WayofTime.bloodmagic.core.RegistrarBloodMagicBlocks;
 import hellfirepvp.astralsorcery.common.block.BlockCustomOre;
 import hellfirepvp.astralsorcery.common.lib.BlocksAS;
 import net.minecraft.item.ItemStack;
@@ -10,8 +11,17 @@ public class OreDictionaryEntries {
     public static void initEntries () {
         ItemStack rockCrystalOre = new ItemStack(BlocksAS.customOre, 1, BlockCustomOre.OreType.ROCK_CRYSTAL.ordinal());
         ItemStack thaumcraftQuartzOre = new ItemStack(BlocksTC.oreQuartz, 1, 0);
+        ItemStack arcaneStone = new ItemStack(BlocksTC.stoneArcane, 1, 0);
+        ItemStack arcaneBricks = new ItemStack(BlocksTC.stoneArcaneBrick, 1, 0);
+        ItemStack stonePorous = new ItemStack(BlocksTC.stonePorous, 1, 0);
+        ItemStack bloodstone = new ItemStack(RegistrarBloodMagicBlocks.DECORATIVE_BRICK, 1, 0);
 
         OreDictionary.registerOre("oreAstralRockCrystal", rockCrystalOre);
         OreDictionary.registerOre("oreThaumcraftQuartz", thaumcraftQuartzOre);
+        OreDictionary.registerOre("arcaneStone", arcaneStone);
+        OreDictionary.registerOre("arcaneStoneBrick", arcaneBricks);
+        OreDictionary.registerOre("stonePorous", stonePorous);
+        OreDictionary.registerOre("bloodstone", bloodstone);
+
     }
 }
