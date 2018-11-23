dbbfe4eff31a162ae3f5d04e0e61ec7c2e7b37e6
Jon McManus <due@wxwhatever.com>
2018-11-23 19:26:36 +1000

    Implement Sun ritual.

--- src/main/java/com/noobanidus/circadian/compat/bloodmagic/rituals/imperfect/SunRitual.java
+++ src/main/java/com/noobanidus/circadian/compat/bloodmagic/rituals/imperfect/SunRitual.java
@@ -1,4 +1,26 @@
 package com.noobanidus.circadian.compat.bloodmagic.rituals.imperfect;
 
-public class SunRitual {
+import WayofTime.bloodmagic.ritual.RitualRegister;
+import WayofTime.bloodmagic.ritual.imperfect.IImperfectRitualStone;
+import WayofTime.bloodmagic.ritual.imperfect.ImperfectRitual;
+import net.minecraft.entity.player.EntityPlayer;
+import vazkii.botania.common.block.ModFluffBlocks;
+
+import javax.annotation.Nonnull;
+
+@RitualRegister.Imperfect("sun")
+@SuppressWarnings("unused")
+public class SunRitual extends ImperfectRitual {
+    public SunRitual() {
+        super("sun", s -> s.getBlock() == ModFluffBlocks.sunnyQuartz, 5000, true, "ritual.circadian.imperfect.sun");
+    }
+
+    @Override
+    public boolean onActivate(@Nonnull IImperfectRitualStone imperfectRitualStone, @Nonnull EntityPlayer player) {
+        if (!imperfectRitualStone.getRitualWorld().isRemote)
+            imperfectRitualStone.getRitualWorld().getWorldInfo().setRaining(false);
+
+        return true;
+    }
 }
+
