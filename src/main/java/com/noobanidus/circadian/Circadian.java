2961cf082a111af27f4223a7448f10a7db14c8d5
Jon McManus <due@wxwhatever.com>
2018-12-12 17:19:47 +1000

    Specify stack sizes of ender pearls, snowballs.

--- src/main/java/com/noobanidus/circadian/Circadian.java
+++ src/main/java/com/noobanidus/circadian/Circadian.java
@@ -95,17 +95,24 @@ public class Circadian {
     public void postInit(FMLPostInitializationEvent e) {
         Mobs.registerSpawns();
         OreDictionaryEntries.initEntries();
+        LootHandler.removeLootEntries();
     }
 
     @Mod.EventHandler
     public void loadComplete(FMLLoadCompleteEvent event) {
+        Blocks.END_PORTAL_FRAME.setHardness(50.0F).setResistance(2000.0F).setHarvestLevel("pickaxe", 3);
+
         int cakeCount = CONFIG.get("Vanilla.Items", "CakeStackSize", 64, "Modify default stack size of cakes.").getInt(64);
         if (cakeCount <= 64 && cakeCount > 0) {
             Items.CAKE.setMaxStackSize(cakeCount);
         }
-        int enderCount = CONFIG.get("Vanilla.Items", "EnderPearlStackSize", 64, "Modify default stack size of ender pearls.").getInt(65);
+        int enderCount = CONFIG.get("Vanilla.Items", "EnderPearlStackSize", 64, "Modify default stack size of ender pearls.").getInt(64);
         if (enderCount <= 64 && enderCount > 0) {
             Items.ENDER_PEARL.setMaxStackSize(enderCount);
+        }
+        int snowballCount = CONFIG.get("Vanilla.Items", "SnowballStackSize", 64, "Modify default stack size of snowballs.").getInt(64);
+        if (snowballCount <= 64 && snowballCount > 0) {
+            Items.SNOWBALL.setMaxStackSize(snowballCount);
         }
 
         RightClickHandler.init();
