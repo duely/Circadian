2c25754d4b5449d85c319c94dad99c63a723be99
Jon McManus <due@wxwhatever.com>
2018-12-12 17:25:46 +1000

    Simplify the process of removing Thaumcraft lootbag entries.
    Just straight up clear it here, instead of having countless lines of
    "remove this item, remove this item" clogging up CraftTweaker.

---
+++ src/main/java/com/noobanidus/circadian/compat/thaumcraft/handlers/LootHandler.java
@@ -0,0 +1,11 @@
+package com.noobanidus.circadian.compat.thaumcraft.handlers;
+
+import thaumcraft.api.internal.WeightedRandomLoot;
+
+public class LootHandler {
+    public static void removeLootEntries () {
+        WeightedRandomLoot.lootBagCommon.clear();
+        WeightedRandomLoot.lootBagUncommon.clear();
+        WeightedRandomLoot.lootBagRare.clear();
+    }
+}
