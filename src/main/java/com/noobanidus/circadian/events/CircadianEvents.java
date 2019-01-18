72f9b82af2721a39063720c066fedbe92f1cc583
Jon McManus <due@wxwhatever.com>
2019-01-19 00:50:52 +1000

    This is buggy for some reason.

--- src/main/java/com/noobanidus/circadian/events/CircadianEvents.java
+++ src/main/java/com/noobanidus/circadian/events/CircadianEvents.java
@@ -5,17 +5,26 @@ import com.google.common.collect.Iterables;
 import com.noobanidus.circadian.Circadian;
 import com.noobanidus.circadian.config.Registrar;
 import com.noobanidus.circadian.enchantment.EnchantmentManabound;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import vazkii.botania.api.mana.IManaUsingItem;
 import vazkii.botania.api.mana.ManaItemHandler;
+import vazkii.botania.common.block.BlockSpecialFlower;
+import vazkii.botania.common.block.tile.TileSpecialFlower;
 
 import java.util.Arrays;
 
@@ -55,6 +64,48 @@ public class CircadianEvents {
 
         if (!world.isRemote && player.ticksExisted % 20 == 0) {
             Registrar.BIOME_TRIGGER.trigger((EntityPlayerMP) player);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onDaisyRightClick (PlayerInteractEvent.RightClickBlock event) {
+        World world = event.getWorld();
+
+        if (!world.isRemote) return;
+
+        EntityPlayer player = event.getEntityPlayer();
+        ItemStack item = player.getHeldItem(event.getHand());
+        BlockPos pos = event.getPos();
+        IBlockState state = world.getBlockState(pos);
+
+        if (item.isEmpty()) return;
+
+        if (!(item.getItem() instanceof ItemBlock)) return;
+
+        if (state.getBlock() instanceof BlockSpecialFlower) {
+            TileSpecialFlower te = (TileSpecialFlower) world.getTileEntity(pos);
+            if (te != null && te.subTileName.equals("puredaisy")) {
+                Block block = ((ItemBlock) item.getItem()).getBlock();
+
+                BlockPos start = pos.add(1, 0, 1);
+                BlockPos stop = pos.add(-1, 0, -1);
+                for (BlockPos potential : BlockPos.getAllInBox(stop, start)) {
+                    if (player.getDistance((double) pos.getX(), (double)pos.getY(), (double)pos.getZ()) < 1.1) continue;
+
+                    IBlockState stateAt = world.getBlockState(potential);
+                    Block blockAt = stateAt.getBlock();
+
+                    if ((blockAt.isReplaceable(world, pos) || blockAt.isAir(stateAt, world, pos)) && block.canPlaceBlockAt(world, potential)) {
+                        IBlockState placingState = block.getStateFromMeta(item.getMetadata());
+                        world.setBlockState(potential, placingState, 1 | 2);
+                        item.shrink(1);
+						ItemBlock.setTileEntityNBT(world, null, potential, item);
+						event.setUseBlock(Event.Result.DENY);
+						event.setUseItem(Event.Result.DENY);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
